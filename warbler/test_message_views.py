abaf37a79e857d9f2145cba91ca581fff87a88dc
SUBstylee <jjthrelfall@gmail.com>
2021-08-31 03:11:02 +0800

    added tests

--- warbler/test_message_views.py
+++ warbler/test_message_views.py
@@ -5,6 +5,7 @@
 #    FLASK_ENV=production python -m unittest test_message_views.py
 
 
+from app import app, CURR_USER_KEY
 import os
 from unittest import TestCase
 
@@ -20,7 +21,15 @@ os.environ['DATABASE_URL'] = "postgresql:///warbler-test"
 
 # Now we can import app
 
-from app import app, CURR_USER_KEY
+
+# don't use Flask DebugToolbar
+app.config['DEBUG_TB_HOSTS'] = ['dont-show-debug-toolbar']
+
+# Flask errors are real errors, instead of HTML pages with error info
+app.config['TESTING'] = True
+
+# turn off debugtoolbar intercept redirects
+app.config['DEBUG_TB_INTERCEPT_REDIRECTS'] = False
 
 # Create our tables (we do this here, so we only create the tables
 # once for all tests --- in each test, we'll delete the data
@@ -51,6 +60,11 @@ class MessageViewTestCase(TestCase):
 
         db.session.commit()
 
+    def tearDown(self):
+        '''clean up fouled transactions'''
+
+        db.session.rollback()
+
     def test_add_message(self):
         """Can use add a message?"""
 
@@ -71,3 +85,113 @@ class MessageViewTestCase(TestCase):
 
             msg = Message.query.one()
             self.assertEqual(msg.text, "Hello")
+
+            self.assertEqual(Message.query.count(), 1)
+
+    def test_add_message_logged_out(self):
+        '''Can a logged out user add a message?'''
+
+        # added follow_redirects so status code is 200
+        with self.client as c:
+            resp = c.post('/messages/new',
+                          data={"text": "Hello"}, follow_redirects=True)
+        html = resp.get_data(as_text=True)
+
+        self.assertEqual(resp.status_code, 200)
+        self.assertIn('Access unauthorized.', html)
+
+    def test_add_empty_message(self):
+        '''Can user add a message if required field left blank?'''
+
+        with self.client as c:
+            with c.session_transaction() as sess:
+                sess[CURR_USER_KEY] = self.testuser.id
+
+            resp = c.post('/messages/new',
+                          data={"text": ""}, follow_redirects=True)
+            html = resp.get_data(as_text=True)
+
+            self.assertEqual(resp.status_code, 200)
+            self.assertIn('Add my message!', html)
+            self.assertEqual(Message.query.count(), 0)
+
+    def test_message_shown(self):
+        '''Are messages being shown?'''
+
+        with self.client as c:
+
+            message_data = {
+                'user_id': self.testuser.id,
+                'text': 'test_message'
+            }
+
+            message = Message(**message_data)
+            db.session.add(message)
+            db.session.commit()
+
+            resp = c.get(f'/messages/{message.id}')
+            html = resp.get_data(as_text=True)
+
+            self.assertEqual(resp.status_code, 200)
+            self.assertIn('test_message', html)
+            self.assertEqual(Message.query.count(), 1)
+
+    def test_message_delete(self):
+        '''Are messages being deleted?'''
+
+        with self.client as c:
+
+            # delete message made before
+            resp = c.post(
+                f'/messages/1/delete', follow_redirects=True)
+            html = resp.get_data(as_text=True)
+
+            self.assertEqual(resp.status_code, 200)
+            self.assertNotIn('test_message', html)
+            self.assertEqual(Message.query.count(), 0)
+
+    def test_different_user_message_delete(self):
+        '''Can a message be deleted by a different user?'''
+
+        testuser2 = User.signup(username='testuser2',
+                                email='test2@test.com',
+                                password='testuser2',
+                                image_url=None)
+        db.session.commit()
+
+        message_data = {
+            'user_id': self.testuser.id,
+            'text': 'test_message'
+        }
+
+        with self.client as c:
+            with c.session_transaction() as sess:
+                sess[CURR_USER_KEY] = testuser2.id
+
+            message = Message(**message_data)
+            db.session.add(message)
+            db.session.commit()
+
+            message = Message(**message_data)
+            db.session.add(message)
+            db.session.commit()
+
+            resp = c.post(
+                f'/messages/{message.id}/delete',
+                follow_redirects=True)
+            html = resp.get_data(as_text=True)
+
+            self.assertEqual(resp.status_code, 200)
+            self.assertEqual(Message.query.count(), 1)
+
+    def test_logged_out_message_delete(self):
+        '''Can a logged out user delete a message?'''
+
+        with self.client as c:
+            resp = c.post(
+                f'/messages/1/delete',
+                follow_redirects=True)
+            html = resp.get_data(as_text=True)
+
+            self.assertEqual(resp.status_code, 200)
+            self.assertIn('Access unauthorized.', html)
--- warbler/test_user_model.py
+++ warbler/test_user_model.py
@@ -5,10 +5,15 @@
 #    python -m unittest test_user_model.py
 
 
+from app import app
 import os
 from unittest import TestCase
 
-from models import db, User, Message, Follows
+from models import db, User, Message, Follows, Likes
+
+from flask_bcrypt import Bcrypt
+# needed for testing unique value error
+from sqlalchemy.exc import IntegrityError
 
 # BEFORE we import our app, let's set an environmental variable
 # to use a different database for tests (we need to do this
@@ -20,13 +25,32 @@ os.environ['DATABASE_URL'] = "postgresql:///warbler-test"
 
 # Now we can import app
 
-from app import app
 
 # Create our tables (we do this here, so we only create the tables
 # once for all tests --- in each test, we'll delete the data
 # and create fresh new clean test data
 
 db.create_all()
+bcrypt = Bcrypt()
+
+U_DATA1 = {
+    'email': 'test1@test.com',
+    'username': 'testuser1',
+    'password': 'HASHED_PASSWORD1'
+}
+
+U_DATA2 = {
+    'email': 'test2@test.com',
+    'username': 'testuser2',
+    'password': 'HASHED_PASSWORD2'
+}
+
+U_DATA3 = {
+    'email': 'test3@test.com',
+    'username': 'testuser3',
+    'password': 'HASHED_PASSWORD3',
+    'image_url': User.image_url.default.arg
+}
 
 
 class UserModelTestCase(TestCase):
@@ -41,18 +65,101 @@ class UserModelTestCase(TestCase):
 
         self.client = app.test_client()
 
+        u = User(**U_DATA1)
+        u2 = User(**U_DATA2)
+
+        db.session.add(u)
+        db.session.add(u2)
+        db.session.commit()
+
+        # changed this to storing self.user_id instead of the instance itself
+        ##############
+        self.user = u
+        self.user2 = u2
+        ##############
+
+    def tearDown(self):
+        '''Clean up fouled transactions'''
+
+        db.session.rollback()
+
     def test_user_model(self):
         """Does basic model work?"""
 
-        u = User(
-            email="test@test.com",
-            username="testuser",
-            password="HASHED_PASSWORD"
-        )
+        # moved some of this to setUp
 
-        db.session.add(u)
+        # User should have no messages, no followers, no likes, and not be following any other users
+        self.assertEqual(len(self.user.messages), 0)
+        self.assertEqual(len(self.user.followers), 0)
+        self.assertEqual(len(self.user.following), 0)
+        self.assertEqual(len(self.user.likes), 0)
+
+    def test_user_following(self):
+        '''Does is_following successfully detect when user1 is/isn't following user2?'''
+        # user is following
+        self.user.following.append(self.user2)
+        self.assertTrue(self.user.is_following(self.user2))
+
+        # user is not following
+        self.user.following.remove(self.user2)
+        self.assertFalse(self.user.is_following(self.user2))
+
+    def test_user_followed(self):
+        '''Does is_followed_by successfully detect when user1 is/isnt followed by user2?'''
+        # user is followed
+        self.user.followers.append(self.user2)
+        self.assertTrue(self.user.is_followed_by(self.user2))
+
+        # user is not followed
+        self.user.followers.remove(self.user2)
+        self.assertFalse(self.user.is_followed_by(self.user2))
+
+    def test_user_signup(self):
+        '''Does User.create successfully create a new user given valid credentials?'''
+
+        new_signup = User.signup(**U_DATA3)
         db.session.commit()
 
-        # User should have no messages & no followers
-        self.assertEqual(len(u.messages), 0)
-        self.assertEqual(len(u.followers), 0)
+        self.assertIsInstance(new_signup.id, int)
+        self.assertEqual(User.query.count(), 3)
+
+    def test_user_signup_fail(self):
+        '''Does User.create fail to create a new user if any of the validations (e.g. uniqueness, non-nullable fields) fail?'''
+
+        # signup fails if required value left empty
+        U_DATA3.pop('username')
+        with self.assertRaises(TypeError):
+            new_user = User.signup(**U_DATA3)
+
+        self.assertEqual(User.query.count(), 2)
+
+        # signup fails if username not unique
+        U_DATA3['username'] = U_DATA1['username']
+        with self.assertRaises(IntegrityError):
+            new_user = User.signup(**U_DATA3)
+            db.session.commit()
+
+        db.session.rollback()
+        self.assertEqual(User.query.count(), 2)
+
+    def test_user_authenticate(self):
+        '''Does User.authenticate successfully return a user when given a valid username and password?  Does User.authenticate fail to return a user when the username or password is invalid?'''
+
+        # needed to hash password and compare
+        pw = U_DATA3['password']
+        U_DATA3['password'] = bcrypt.generate_password_hash(
+            U_DATA3['password']).decode('UTF-8')
+        new_user = User(**U_DATA3)
+
+        db.session.add(new_user)
+        db.session.commit()
+
+        # test if authenticate is successful with correct username and password
+        self.assertEqual(User.authenticate(U_DATA3['username'], pw), new_user)
+
+        # test if authenticate fails on wrong password
+        self.assertFalse(User.authenticate(
+            U_DATA3['password'], 'wrongpassword'))
+
+        # test if authenticate fails on wrong username
+        self.assertFalse(User.authenticate('wrongusername', pw))

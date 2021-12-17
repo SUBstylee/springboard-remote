fa17d6c667e2ffdf817266a3761698cbfc049680
SUBstylee <jjthrelfall@gmail.com>
2021-12-18 02:32:07 +0800

    commented and cleaned up helper functions

--- react/react-cra/es2015-module-exercise/src/helpers.js
+++ react/react-cra/es2015-module-exercise/src/helpers.js
@@ -1,9 +1,11 @@
 function choice(items) {
+    //randomly chooses then returns one of the items from the array passed into it
     const idx = Math.floor(Math.random() * items.length);
     return items[idx];
 };
 
 function remove(items, item) {
+    //removes an 'item' from the 'items' in the array. - Using filter, creates and returns a new array without 'item' in it.
     let remainingFruit = [];
     items.filter(i => {
         if (i !== item) {

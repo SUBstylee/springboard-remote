6c62bd5cf28d199b57ea95893f50b73b792729b0
SUBstylee <jjthrelfall@gmail.com>
2021-12-21 02:53:23 +0800

    magic eight ball

--- react/react-events-state/eightball/src/EightBall.js
+++ react/react-events-state/eightball/src/EightBall.js
@@ -3,31 +3,57 @@ import './EightBall.css';
 
 const EightBall = (props) => {
     let ansColor = '';
+
     const randAns = () => props.answers[Math.floor(Math.random() * props.answers.length)];
     const showAnswer = () => {
         const ans = randAns();
         setAns(ans);
-        ans.color === 'green' ? ansColor = 'green' : (ans.color === 'red' ? ansColor = 'red' : ansColor = 'yellow');
-        console.log(ansColor);
+
+        if (ans.color === 'green') {
+            ansColor = 'EightBall-green';
+            setPositive(positive + 1)
+        } else if (ans.color === 'red') {
+            ansColor = 'EightBall-red';
+            setNegative(negative + 1);
+        } else {
+            ansColor = 'EightBall-yellow';
+            setNeutral(neutral + 1);
+        }
+        setTotal(total + 1);
+        setBgcolor(`EightBall-center ${ansColor}`);
+    };
+    const reset = () => {
+        setAns('');
+        setBgcolor('EightBall-center EightBall-black');
+        setPositive(0);
+        setNeutral(0);
+        setNegative(0);
+        setTotal(0);
     };
 
-    const [ans, setAns] = useState(randAns());
+    const [ans, setAns] = useState('');
+    const [bgcolor, setBgcolor] = useState('EightBall-center EightBall-black');
+    const [positive, setPositive] = useState(0);
+    const [neutral, setNeutral] = useState(0);
+    const [negative, setNegative] = useState(0);
+    const [total, setTotal] = useState(0);
     return (
         <div className="EightBall">
             <h1>Magic Eight Ball</h1>
             <h2>Concentrate on your question and ask the Eight Ball</h2>
-            {/* <div className="EightBall-tally">
-                <ul>
-                    <li id={1}>Positive: </li>
-                    <li id={2}>Neutral: </li>
-                    <li id={3}>Negative: </li>
-                </ul>
-            </div> */}
-            <div className="EightBall-ball">
-                <p className="EightBall-answer">{ans.msg}</p>
+            <div className="EightBall-tally">
+                <p className="EightBall-positive">Positive: {positive}</p>
+                <p className="EightBall-neutral">Neutral: {neutral}</p>
+                <p className="EightBall-negative">Negative: {negative}</p>
+                <p className="EightBall-total">Total: {total}</p>
+            </div>
+            <div className='EightBall-ball'>
+                <div className={bgcolor}>
+                    <p className="EightBall-answer">{ans.msg}</p>
+                </div>
             </div>
             <button onClick={() => showAnswer()}>What is the answer?</button>
-            {/* <button>Reset</button> */}
+            <button onClick={() => reset()}>Reset</button>
         </div>
     );
 };

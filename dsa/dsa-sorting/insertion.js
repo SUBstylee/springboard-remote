343fc100eaef4e339cab287d68ab4d09c13ff0c4
SUBstylee <jjthrelfall@gmail.com>
2021-12-31 12:33:45 +0800

    Insertion Sort

--- dsa/dsa-sorting/insertion.js
+++ dsa/dsa-sorting/insertion.js
@@ -12,7 +12,7 @@
 // function insertionSort(arr) {
 //     for (let i = 1; i < arr.length; i++) {
 //         let curVal = arr[i];
-//         var j = i - 1; //need j to be declared here or will be outside of scope for call after looping
+//         let j = i - 1; //need j to be declared here or will be outside of scope for call after looping
 //         for (; j >= 0 && arr[j] > curVal; j--) {
 //             arr[j + 1] = arr[j];
 //         };
@@ -21,16 +21,15 @@
 //     return arr;
 // };
 
-function insertionSort(arr) {
-    for (let i = 1; i < arr.length; i++) {
-        let curVal = arr[i];
-        let index = i;
-        for (let j = i - 1; j >= 0 && arr[j] > curVal; j--) {//need j to be var for hoisting
-            [arr[j], arr[j + 1]] = [curVal, arr[j]]
-        };
-    };
-    return arr;
-};
+// function insertionSort(arr) {
+//     for (let i = 1; i < arr.length; i++) {
+//         let curVal = arr[i];
+//         for (let j = i - 1; j >= 0 && arr[j] > curVal; j--) {
+//             [arr[j], arr[j + 1]] = [curVal, arr[j]];
+//         };
+//     };
+//     return arr;
+// };
 
 
 module.exports = insertionSort;

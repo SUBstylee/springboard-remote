561e72328b8c718ee983933ef8b036c3177d582a
SUBstylee <jjthrelfall@gmail.com>
2022-01-07 04:36:00 +0800

    Quick and Radix sorts

--- dsa/dsa-sorting/radix.js
+++ dsa/dsa-sorting/radix.js
@@ -1,3 +1,34 @@
-function radixSort() {}
+function radixSort(arr) {
+    const numLoops = mostDigits(arr);
+    for (let k = 0; k < numLoops; k++) {
+        let buckets = Array.from({ length: 10 }, () => []);
+        for (let i = 0; i < arr.length; i++) {
+            buckets[getDigit(arr[i], k)].push(arr[i]);
+        };
+        arr = [].concat(...buckets);
+    };
+    return arr;
 
-module.exports = radixSort;
+}
+
+//helper functions
+function getDigit(num, i) {
+    return Math.floor(Math.abs(num) / Math.pow(10, i)) % 10;
+};
+
+function digitCount(num) {
+    if (num === 0) return 1;
+    return Math.floor(Math.log10(Math.abs(num))) + 1;
+    //alternatively
+    // return num.toString().length;
+};
+
+function mostDigits(arr) {
+    let most = 0;
+    for (let i = 0; i < arr.length; i++) {
+        most = Math.max(most, digitCount(arr[i]));
+    };
+    return most;
+};
+
+module.exports = { radixSort, getDigit, digitCount, mostDigits };

9f998d849b179c295de41cb2183fff7eb988927b
SUBstylee <jjthrelfall@gmail.com>
2022-02-27 10:02:30 +0800

    classname changes on header

--- redux/redux-microblog/frontend/src/components/Header/Header.jsx
+++ redux/redux-microblog/frontend/src/components/Header/Header.jsx
@@ -2,11 +2,11 @@ import { NavLink } from "react-router-dom";
 import './Header.css'
 
 const Header = () => (
-    <div className="Header container">
+    <div className="Header text-center">
         <div className="jumbotron">
             <header className="App-header mt-2">
-                <h1 className="App-title display-4">Microblog</h1>
-                <p className="lead">Get in the Rithm of blogging!</p>
+                <h1 className="App-title display-4">MicroBlog</h1>
+                <p className="lead">Anonymous blog!</p>
                 <nav>
                     <NavLink exact to="/">Blog</NavLink>
                     <NavLink exact to="/new">Add a new post</NavLink>
